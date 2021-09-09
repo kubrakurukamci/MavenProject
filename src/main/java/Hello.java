f32911d477faa159bc36696e38e88413c1db5df6
kubrakurukamci <kubratrh1453@gmail.com>
2021-09-09 11:05:28 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("LOcal repo is already published to gitHup ");
     }
 }
